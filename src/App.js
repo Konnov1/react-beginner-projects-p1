29ea6162e0645f573090523619af623e01a75523
Konnov1 <mkonnov155@gmail.com>
2026-02-24 20:59:35 +0000

    p3

--- src/App.js
+++ src/App.js
@@ -6,9 +6,24 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+  fetch('https://reqres.in/api/users')
+  .then(res => res.json())
+  .then(json => {
+    setUsers(json.record.data); 
+  })
+    .catch((err) => {
+      console.warn(err);
+      alert('Ошибка при получении пользователей');
+    })
+    .finally(() => setLoading(false));
+ }, []);
+
   return (
     <div className="App">
-      <Users />
+      <Users users={users} isLoading={isLoading}/>
       {/* <Success /> */}
     </div>
   );
